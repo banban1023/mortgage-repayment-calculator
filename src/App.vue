69b53c8d1d674d702dc855a9b3b2711f22d508ef
板板 <747657490@qq.com>
2025-07-01 17:58:41 +0800

    准备工作完成

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="calculator">
+    111
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+@import './styles/common';
+</style>
